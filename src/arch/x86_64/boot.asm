4c12a52c16db90292673a060a7cbfd4051d11636
Calle Malmz <callem217@gmail.com>
2017-04-22 16:14:07 +0200

    Add print functionallity to vga buffer

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -5,6 +5,7 @@ section .text
 bits 32
 start:
 	mov esp, stack_top
+	mov edi, ebx
 
 	call check_multimoot
 	call check_cpuid
@@ -138,7 +139,7 @@ p3_table:
 p2_table:
 	resb 4096
 stack_bottom:
-	resb 64
+	resb 4096 * 4
 stack_top:
 
 section .rodata
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,47 @@
-#![feature(lang_items)]
+#![feature(lang_items, const_fn, unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+extern crate multiboot2;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() {
-	let x = ["Hello", "World", "!"];
-	let y = x;
+pub extern fn rust_main(multiboot_information_adress: usize) {
+	let boot_info = unsafe{ multiboot2::load(multiboot_information_adress) };
+	vga_buffer::clear_screen();
+	
+	let memory_map_tag = boot_info.memory_map_tag()
+		.expect("Memory map tag required");
+	
+	println!("Memory areas:");
+	for area in memory_map_tag.memory_areas() {
+		println!("\tstart: 0x{:x}, length: 0x{:x}",
+			area.base_addr, area.length);
+	}
+
+	/*
+	let elf_sections_tag = boot_info.elf_sections_tag()
+		.expect("Elf-sections tag required");
+	println!("Kernel sections:");
+	for sections in elf_sections_tag.sections() {
+		println!("\taddr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+			sections.addr, sections.size, sections.flags);
+	}
+	*/
+
+	loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+	println!("\n\nPANIC in {} at line {}:", file, line);
+	println!("\t{}", fmt);
+	loop{}
+}
