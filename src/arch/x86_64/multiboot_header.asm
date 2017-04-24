ca3e72cea7ff98ca19c2547db05f88754ad49a92
Calle Malmz <callem217@gmail.com>
2017-04-24 21:36:10 +0200

    Add some documentation to multiboot header

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -1,11 +1,14 @@
 section .multiboot_header
 header_start:
-	dd 0xe85250d6
-	dd 0
-	dd header_end - header_start
+	dd 0xe85250d6 ; Magic number
+	dd 0 ; Protected mode code
+	dd header_end - header_start ; header length
+
+	; Checksum
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-	dw 0
-	dw 0
-	dd 8
+	; End data tags
+	dw 0 ; type
+	dw 0 ; flags
+	dd 8 ; size
 header_end:
